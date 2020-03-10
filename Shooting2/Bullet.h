42fc078923739506850583b58b85050495c52ed8
조윤영 <iop01447@naver.com>
2020-03-10 15:28:01 +0900

    bullet 틀

---
+++ Shooting2/Bullet.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Obj.h"
+class CBullet :
+	public CObj
+{
+public:
+	CBullet();
+	virtual ~CBullet();
+
+	// CObj을(를) 통해 상속됨
+	virtual void Initialize() override;
+	virtual int Update() override;
+	virtual void Late_Update() override;
+	virtual void Render(HDC hDC) override;
+	virtual void Release() override;
+
+private:
+
+};
+
